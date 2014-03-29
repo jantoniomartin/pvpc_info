84e7aa8a7ceb331b55d2ac29df7e8003a9ed1cb2
José Antonio Martín <jantonio.martin@gmail.com>
2014-03-29 14:59:11 +0100

    disable admin urls

--- pvpc_info/urls.py
+++ pvpc_info/urls.py
@@ -10,7 +10,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'pvpc_info.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    #url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('prices.urls')),
 )
 
